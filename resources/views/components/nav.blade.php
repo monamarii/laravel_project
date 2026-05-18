45ea95d22445bfe33136a55cd90729f9d4819b2c
Mona-Marii Kokk <monamariik@gmail.com>
2026-05-18 20:19:34 +0300

    edit

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -10,7 +10,7 @@
             <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li><a href="/ideas/">your ideas</a></li>
                 <li><a href="/ideas/create">new idea</a></li>
-                <li><a href="/cats">cats</a></li>
+                <li><a href="/frogs">frogs</a></li>
             </ul>
         </div>
         <a class="btn btn-ghost text-xl" href="/frogs">Frogs & stuff</a>
